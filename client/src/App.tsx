23ff8e4ac29cdf46e3f5adc9033493145ba8c53b
evan-colorkrew <evertjannes.degraaff@colorkrew.com>
2024-06-21 10:25:59 +0900

    Create mock as exported const

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, CircularProgress } from "@mui/material";
+import { Box } from "@mui/material";
 import "./App.css";
 import { Layout } from "./components/layout/Layout";
 import { UserSideMenu } from "./components/widgets/UserSideMenu";
@@ -6,61 +6,8 @@ import { SelectedUserProvider } from "./components/context/SelectedUserContext";
 import { ScoreChart } from "./components/widgets/ScoreChart";
 import { BehaviorScore } from "./components/widgets/BehaviorScore";
 import { Information } from "./components/widgets/Information";
-import { useEffect, useState } from "react";
-
-type UserStatusStatus = 'normal' | 'review' | 'required';
-type UserStatus = {
-  user: number;
-  status: UserStatusStatus;
-}
 
 function App() {
-  const [scoreData, setScoreData] = useState<any>(undefined);
-  const [usersStatus, setUsersStatus] = useState<UserStatus[]>([]);
-
-  useEffect(() => {
-    fetch('/mock.json')
-      .then((response) => response.json())
-      .then((data: any) => {
-        setScoreData(data)
-        const userStatusData: UserStatus[] = [];
-        
-        for (let i = 0; i < data.length; i++) {
-          const userId = data[i].userId;
-          const latestScore = data[i].scores[data[i].scores.length - 1].zScore;
-          
-          let status: UserStatusStatus = 'normal';
-          if (latestScore > 2) {
-            status = 'review';
-          }
-          if (latestScore > 3) {
-            status = 'required';
-          }
-
-          userStatusData.push({
-            user: userId,
-            status: status
-          });
-        }
-        
-        setUsersStatus(userStatusData)
-      });
-  }, [setScoreData])
-
-  if (!scoreData && !usersStatus) {
-    return (<Box
-      sx={{
-        alignItems: "center",
-        display: "flex",
-        height: "100%",
-        justifyContent: "center",
-        width: "100%"
-      }}
-    >
-      <CircularProgress />
-    </Box>);
-  }
-
   return (
     <SelectedUserProvider>
       <Layout>
